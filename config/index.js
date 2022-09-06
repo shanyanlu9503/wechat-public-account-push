19b90a3b5bcf25436238e2666e45ea2de04036ca
shanyanlu9503 <33244144+shanyanlu9503@users.noreply.github.com>
2022-09-06 17:08:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -33,7 +33,7 @@ export const config = {
       // 想要发送的人的名字
       name: "宝宝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oINVB56pIrx7ANgmm1XIFLY-VPKA",
+      id: "oINVB5xiCD3INIh80uGVD8mU3ark",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "Oe1H00i9PA0O2WxljAgCNweQtr50wokMWNsC4vOx8_8",
       // 所在省份
